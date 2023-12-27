f5896bc2664ba32f0945d7c490177d588c67c498
seoboin <sbi1024@naver.com>
2023-12-27 23:09:05 +0900

    level0 commit 23.12.27 -2

---
+++ src/level0/Level0_문자열잘라서정렬하기.java
@@ -0,0 +1,26 @@
+package level0;
+
+import java.util.Arrays;
+
+public class Level0_문자열잘라서정렬하기 {
+    public static void main(String[] args) {
+        String[] solution1 = solution("axbxcxdx");
+        System.out.println("Arrays.toString(solution1) = " + Arrays.toString(solution1));
+
+        String[] solution2 = solution("dxccxbbbxaaaa");
+        System.out.println("Arrays.toString(solution2) = " + Arrays.toString(solution2));
+
+        String[] solution3 = solution("zxcxbxaxxxxxxx");
+        System.out.println("Arrays.toString(solution3) = " + Arrays.toString(solution3));
+
+        String[] solution4 = solution("dxccxbbbxxaaaa");
+        System.out.println("Arrays.toString(solution4) = " + Arrays.toString(solution4));
+    }
+
+    public static String[] solution(String myString) {
+        return Arrays.stream(myString.split("x"))
+                .filter(value -> !value.isEmpty())
+                .sorted()
+                .toArray(String[]::new);
+    }
+}
