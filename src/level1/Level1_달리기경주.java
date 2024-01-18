2bfbaca29c2f78c27a000d14d00033b4e3939603
seoboin <sbi1024@naver.com>
2024-01-18 23:10:49 +0900

    level1 commit 24.01.18 -3

---
+++ src/level1/Level1_달리기경주.java
@@ -0,0 +1,22 @@
+package level1;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Level1_달리기경주 {
+    public static void main(String[] args) {
+        String[] test1 = {"mumu", "soe", "poe", "kai", "mine"};
+        String[] test2 = {"kai", "kai", "mine", "mine"};
+        String[] solution1 = solution(test1, test2);
+        System.out.println("Arrays.toString(solution1) = " + Arrays.toString(solution1)); // ["mumu", "kai", "mine", "soe", "poe"]
+    }
+
+    public static String[] solution(String[] players, String[] callings) {
+        String[] answer = {};
+
+        // 아이디어 : 랭크 : 키
+
+        return answer;
+    }
+}
